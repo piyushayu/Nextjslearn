6d8da7b911c4d9698be1d0a79526c0930bb297d1
piyushayu <piyush.ps1412@outlook.com>
2026-08-11 17:24:44 +0530

    addedsomefrontendpartalso

---
+++ my-app/src/app/(auth)/sign-in/page.tsx
@@ -0,0 +1,14 @@
+'use client'
+
+import { useSession , signIn , signOut } from "next-auth/react"
+
+export default function Component() {
+    const {data : session} = useSession()
+    if(session){
+    return (
+        <div>
+            <h1>Sign In</h1>
+        </div>
+    )
+}
+}
